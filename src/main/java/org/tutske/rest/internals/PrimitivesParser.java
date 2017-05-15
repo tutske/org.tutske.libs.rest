ba442bf710f264f19690067c19cef14d0e220952
Jeroen Matthijssens <jeroen.matthijssens@gmail.com>
2017-05-15 09:23:31 +0200

    [RST-1] Teach primitives to parse dates.

--- src/main/java/org/tutske/rest/internals/PrimitivesParser.java
+++ src/main/java/org/tutske/rest/internals/PrimitivesParser.java
@@ -1,11 +1,21 @@
 package org.tutske.rest.internals;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
 
 public class PrimitivesParser {
+
+	private static final SimpleDateFormat [] formats = new SimpleDateFormat [] {
+		new SimpleDateFormat ("yyyy-MM-dd'T'HH:mm:ss'Z'Z"),
+		new SimpleDateFormat ("yyyy/MM/dd"),
+		new SimpleDateFormat ("dd/MM/yyyy"),
+		new SimpleDateFormat ("dd/MM/yy"),
+	};
 
 	private static final String ERROR_FORMAT = "Clazz not supported for conversion: %s (%s)";
 
@@ -18,6 +28,7 @@ public class PrimitivesParser {
 		converters.put (Float.class, Float::parseFloat);
 		converters.put (Double.class, Double::parseDouble);
 		converters.put (Boolean.class, Boolean::parseBoolean);
+		converters.put (Date.class, PrimitivesParser::parseDate);
 	}
 
 	public static <T> T parse (String value, Class<T> clazz) {
@@ -27,6 +38,18 @@ public class PrimitivesParser {
 		}
 
 		return (T) converters.get (clazz).apply (value);
+	}
+
+	public static Date parseDate (String value) {
+		for ( SimpleDateFormat format : formats ) {
+			try { return format.parse (value); }
+			catch ( ParseException ignore ) {}
+		}
+
+		try { return new Date (Long.parseLong (value)); }
+		catch ( NumberFormatException ignore ) {}
+
+		throw new IllegalArgumentException ("Not a valid date representation: `" + value + "`");
 	}
 
 }
