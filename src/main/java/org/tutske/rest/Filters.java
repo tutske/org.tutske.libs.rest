b1a2ba8be7a356b30dbfb61bc1725d060177d069
Groot Sarchy <groot.sarchy@gmail.com>
2019-02-06 19:04:37 +0100

    [ts-4] Add default origin filter.

---
+++ src/main/java/org/tutske/rest/Filters.java
@@ -0,0 +1,18 @@
+package org.tutske.rest;
+
+
+public class Filters {
+
+	public static <REQ extends Request, RES> Filter<REQ, RES> allowOriginFilter (String origin) {
+		return (req, chain) -> {
+			if ( req.getHeader ("origin") != null ) {
+				req.setHeader ("Access-Control-Allow-Origin", origin);
+				req.setHeader ("Access-Control-Allow-Credentials", "true");
+				req.setHeader ("Access-Control-Allow-Methods", req.getHeader ("Access-Control-Request-Method"));
+				req.setHeader ("Access-Control-Allow-Headers", req.getHeader ("Access-Control-Request-Headers"));
+			}
+			return chain.apply (req);
+		};
+	}
+
+}
